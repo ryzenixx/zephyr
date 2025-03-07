12a17d1fa6901e0b5c548864596cd3465538108e
Mael <96339570+ryzenixx@users.noreply.github.com>
2025-03-07 20:13:58 +0100

    Ajout de sous dossiers pour les commandes

---
+++ commands/moderation/kick.js
@@ -0,0 +1,123 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "kick",
+    description: "Exclure un utilisateur de ce serveur",
+    permission: Discord.PermissionFlagsBits.KickMembers,
+    dm: false,
+    options: [
+        {
+            type: "user",
+            name: "utilisateur",
+            description: "Nom de l'utilisateur à exclure",
+            required: true
+        },
+        {
+            type: "string",
+            name: "raison",
+            description: "Raison de l'exclusion",
+            required: false
+        },
+    ],
+
+    async run(bot, interaction) {
+        
+        let db = bot.db;
+        const guildId = interaction.guild.id;
+
+        const sql = `SELECT mod_commands_enabled FROM guild_settings WHERE guild_id = ?`;
+        db.query(sql, [guildId], async (err, result) => {
+
+            if(err) {
+                console.error(err);
+                return interaction.reply({ content: "**Une erreur s'est produite lors de la récupération des paramètres pour votre serveur. ❌**", ephemeral: true });
+            }
+
+            let ModCommandsEnabled = false;
+
+            if(result.length > 0) {
+                ModCommandsEnabled = result[0].mod_commands_enabled;
+            }
+
+            if(ModCommandsEnabled){
+                
+                const utilisateur = interaction.options.getUser('utilisateur');
+                const raison = interaction.options.getString('raison') || 'Aucune raison fournie.';
+
+                let member;
+                try {
+                    member = await interaction.guild.members.fetch(utilisateur.id);
+                } catch (error) {
+
+                    const embedNotInTheServer = new Discord.EmbedBuilder()
+                        .setColor(0xf56868)
+                        .setTitle("Oups, une erreur s'est produite... ❌")
+                        .setDescription(`L'utilisateur \`\`${utilisateur.tag}\`\` n'est pas dans ce serveur.`)
+                        .setTimestamp()
+                        .setFooter({ text: 'Zéphyr - Votre assistant Discord', iconURL: bot.user.displayAvatarURL() });
+
+                    return interaction.rely({ embeds: [embedNotInTheServer] });
+
+                }
+
+                if(!member.kickable) {
+                    const embedNotKickable = new Discord.EmbedBuilder()
+                        .setColor(0xf56868)
+                        .setTitle("Oups, une erreur s'est produite... ❌")
+                        .setDescription(`Je ne suis pas en mesure d'exclure \`\`${utilisateur.tag}\`\`. Vérifiez mes permissions et autorisations sur ce serveur.`)
+                        .setTimestamp()
+                        .setFooter({ text: 'Zéphyr - Votre assistant Discord', iconURL: bot.user.displayAvatarURL() });
+
+                    return interaction.reply({ embeds: [embedNotKickable] })
+                }
+
+                if(!member) {
+                    const embedNotInTheServer = new Discord.EmbedBuilder()
+                        .setColor(0xf56868)
+                        .setTitle("Oups, une erreur s'est produite... ❌")
+                        .setDescription(`L'utilisateur \`\`${utilisateur.tag}\`\` n'est pas dans ce serveur.`)
+                        .setTimestamp()
+                        .setFooter({ text: 'Zéphyr - Votre assistant Discord', iconURL: bot.user.displayAvatarURL() });
+
+                    return interaction.rely({ embeds: [embedNotInTheServer] });
+                }
+
+                const embedKicked = new Discord.EmbedBuilder()
+                    .setColor(0x9cff8e)
+                    .setTitle(`${utilisateur.tag} **a été expulsé.** ✅`)
+                    .setDescription(`\n\n**Raison:** ${raison}`)
+                    .setTimestamp()
+                    .setFooter({ text: 'Zéphyr - Votre assistant Discord', iconURL: bot.user.displayAvatarURL() });
+
+                try {
+                    const discordNameButton = new Discord.ActionRowBuilder()
+                    .addComponents(
+                        new Discord.ButtonBuilder()
+                            .setCustomId('01')
+                            .setStyle(Discord.ButtonStyle.Secondary)
+                            .setLabel(`Envoyé depuis: ${interaction.guild.name}`)
+                            .setDisabled(true)
+                    )
+
+                    await utilisateur.send({ content: `🚨 **Tu as été expulsé de ${interaction.guild.name}** 🚨\n📌 **Raison :** ${raison}`, components: [discordNameButton] });
+                } catch (error) {
+                    console.log(`Impossible d'envoyer un DM à ${user.tag}.`);
+                }
+
+                await member.kick(raison);
+                await interaction.reply({ embeds: [embedKicked] })
+
+            }
+            else {
+
+                const embedDisabled = new Discord.EmbedBuilder()
+                    .setColor(0xf56868)
+                    .setDescription("**Les commandes de modération sont désactivées sur ce serveur. ❌**")
+
+                await interaction.reply({ embeds: [embedDisabled] })
+            }
+
+        });
+
+    }
+}
---
+++ commands/utils/help.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "help",
+    description: "Affiche les commandes de Zéphyr",
+    dm: false,
+
+    async run(bot, interaction) {
+        
+        const helpEmbed = new Discord.EmbedBuilder()
+            .setColor(0xffffff)
+            .setTitle("Le guide d'aide de Zéphyr 📖")
+            .setDescription("**⚠️ Le bot est en développement, si vous souhaitez contribuer à de nouvelles idées, [rejoignez notre Discord](https://discord.gg/NE5SQwjJp4) !**\n\n\n**👋 Général**\n</help:1347563329446613093> **-** Affiche les commandes de Zéphyr\n\n**🔄 Utilitaire**\n</invite:1347565523147165696> **-** Invite Zéphyr sur un autre serveur !\n\n**🚨 Modération**\n</kick:1347568140355371019> **-** Exclure un utilisateur de ce serveur\n</ban:1347639177650507919> **-** Bannir un utilisateur de ce serveur\n</mute:1347639177650507920> **-** Réduire au silence un utilisateur de ce serveur\n\n**🎮 Divertissement**\n</blague:1347642736194617384> **-** Affiche une blague aléatoire\n</meme:1347645316169728041> **-** Affiche un même aléatoire\n</8ball:1346271075096334519> **-** Pose une question et la boule magique te répondra !\n\n**⚙️ Configuration**\n</welcome:1346618984388231209> **-** Active/désactive les messages d'arrivées sur ce serveur !\n</autorole:1346626527877660817> **-** Active/désactive l'autorole sur ce serveur !")
+            .setTimestamp()
+            .setFooter({ text: 'Zéphyr - Votre assistant Discord', iconURL: bot.user.displayAvatarURL() });
+
+        interaction.reply({ embeds: [helpEmbed] });        
+
+    }
+}
