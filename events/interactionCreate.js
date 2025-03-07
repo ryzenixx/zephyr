300619818e6bde4440ac4b94ca845aa29871a501
Mael <96339570+ryzenixx@users.noreply.github.com>
2025-03-07 22:11:22 +0100

    Modification de la gestion des commandes dans l'event interactionCreate

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,8 +1,19 @@
+const path = require("path");
 const Discord = require("discord.js");
 
 module.exports = async (bot, interaction) => {
-    if (interaction.type === Discord.InteractionType.ApplicationCommand) {
-      let command = require(`../commands/${interaction.commandName}`)
-      command.run(bot, interaction, command.options)
-    }
-}
+  if (interaction.type === Discord.InteractionType.ApplicationCommand) {
+      const command = bot.commands.get(interaction.commandName);
+
+      if (!command) {
+          return interaction.reply({ content: "Commande non trouvée.", ephemeral: true });
+      }
+
+      try {
+          await command.run(bot, interaction, command.options);
+      } catch (err) {
+          console.error(`Erreur lors de l'exécution de la commande ${interaction.commandName}:`, err);
+          interaction.reply({ content: "Il y a eu une erreur lors de l'exécution de la commande.", ephemeral: true });
+      }
+  }
+};
